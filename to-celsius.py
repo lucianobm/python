d566840802b2ff58400a0aae319a4e8784cb0f5c
Luciano Marcondes <lucianobm@gmail.com>
2023-02-01 02:13:34 +0000

    specifying 2 decimals for celsius

--- to-celsius.py
+++ to-celsius.py
@@ -2,4 +2,5 @@
 
 fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to Celsius? "))
 celsius = (fahrenheit - 32) * 5 / 9
-print(fahrenheit, "F is", celsius, "C")
+
+print(fahrenheit, "F is", round(celsius, 2), "C")
