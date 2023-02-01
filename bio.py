e9a8150f82c5f8fc961dd5c99e7a9de91a099cd4
Luciano Marcondes <lucianobm@gmail.com>
2023-02-01 01:56:57 +0000

    testing input and print

---
+++ bio.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = int(input("How old are you today? "))
+
+# print(name)
+# print("is " + str(age) + " years old")
+# print("and loves the color " + color + ".")
+
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".", end=" ")
