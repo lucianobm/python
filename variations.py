b43debf31a614ba2a671f36e983f943615e4b645
Luciano Marcondes <lucianobm@gmail.com>
2023-02-04 01:30:20 +0000

    working with string methods

---
+++ variations.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+
+message = input("Enter a message: ")
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
